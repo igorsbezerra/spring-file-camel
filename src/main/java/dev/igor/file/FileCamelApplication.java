d830f9c6b868a32e119822617df907b9da203afe
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-07-31 20:14:44 -0300

    first commit

---
+++ src/main/java/dev/igor/file/FileCamelApplication.java
@@ -0,0 +1,13 @@
+package dev.igor.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCamelApplication.class, args);
+    }
+
+}
